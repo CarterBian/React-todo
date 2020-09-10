9453bb7a51524c2fcf08aabe3445870c8de99d2f
Carter Bian <carterbian32@gmail.com>
2020-09-10 00:05:26 -0700

    Updates

---
+++ precache-manifest.8be0bf86b626e1c09293345dbf70ac07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb37bb87ea64aad54df54923b467a29e",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "7be77509c77ceaa61e77",
+    "url": "/React-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "81e71619a4aaa2bc120c",
+    "url": "/React-todo/static/js/2.0af54df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo/static/js/2.0af54df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be77509c77ceaa61e77",
+    "url": "/React-todo/static/js/main.a94758d5.chunk.js"
+  },
+  {
+    "revision": "b1ffc4dbeaf23aacafb9",
+    "url": "/React-todo/static/js/runtime-main.7173269f.js"
+  }
+]);
